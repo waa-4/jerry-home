a260c2e2e620cbfe4460c39a0c7fa118b8294efc
waa-4 <gregorycheney209@gmail.com>
2026-02-05 06:04:44 -0800

    Create plugins.js

---
+++ other/plugins.js
@@ -0,0 +1,13 @@
+// other/plugins.js
+// Add new plugin cards here. Home page will auto-load them.
+
+window.JERRY_PLUGINS = [
+  // Example plugin (replace / remove if you want)
+  {
+    id: "placeholder-tool",
+    title: "Plugin Example",
+    icon: "🧩",
+    description: "This card is loaded from /other/plugins.js",
+    href: "./other/placeholder/" // you can point anywhere
+  }
+];
