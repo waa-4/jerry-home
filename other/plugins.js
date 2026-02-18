5eba0fd662642c08f86536f51196c5f117e6ee95
waa-4 <gregorycheney209@gmail.com>
2026-02-18 12:10:44 -0800

    Update plugins.js

--- other/plugins.js
+++ other/plugins.js
@@ -2,17 +2,18 @@
 // Add new plugin cards here. Home page will auto-load them.
 
 window.JERRY_PLUGINS = [
-  // Example plugin (replace / remove if you want)
   {
     id: "pixelab-tool",
     title: "JERRY Pixel Lab",
     icon: "🖼",
     description: "You can use this to draw with pixels.",
     href: "./pixelart/" // you can point anywhere
+  },
 
+  {
     id: "mapmaker",
     title: "Map Maker",
-    icon: "🖼",
+    icon: "🗺",
     description: "You can create a map with this for free! :D",
     href: "./mapmaker/" // you can point anywhere
   }
